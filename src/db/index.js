e412a3bf698a0f7c153d0de6c3eb9c5f0572df77
vaibhav-dev-kitchery <vaibhav.kitchery@gmail.com>
2024-08-31 13:26:38 +0530

    Database connectivity completed.

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,15 @@
 import { DB_NAME } from "../constants.js";
-
+import mongoose from "mongoose";
 
 const connectDB =  async() => {
-
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.log("MONGO DB connection failed", error);
+        process.exit(1)
+    }
 }
 
 
